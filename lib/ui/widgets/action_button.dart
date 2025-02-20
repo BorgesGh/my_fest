0b2c316afdb192abfb737aa6c4d860623aaf700c
BorgesGh <ghabrielbc@gmail.com>
2025-02-20 14:13:19 -0300

    Adicionar botão de ação

---
+++ lib/ui/widgets/action_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:my_fest/ui/style/pallete.dart';
+
+class ActionButton extends StatelessWidget {
+  String content;
+  Function()? onPressed;
+  ActionButton({super.key, required this.content, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ButtonStyle(
+        elevation: const WidgetStatePropertyAll(10),
+        backgroundColor: const WidgetStatePropertyAll(Pallete.orangeColor),
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        foregroundColor: const WidgetStatePropertyAll(Colors.white),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        content,
+        style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}

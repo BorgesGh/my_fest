8e8bfc38ab753cbb707bb3ef77509c13b57c18c1
BorgesGh <ghabrielbc@gmail.com>
2025-02-20 14:13:50 -0300

    Adicionar standart project

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:my_fest/router.dart';
+import 'package:my_fest/ui/style/pallete.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      routerConfig: goRouter(),
+      debugShowCheckedModeBanner: false,
+      title: 'My Fest',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorScheme:
+            ColorScheme.fromSwatch(primarySwatch: Pallete.primaryColor),
       ),
     );
   }

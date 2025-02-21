dc54e1108b70d695329badb90fa42366b2d72623
BorgesGh <ghabrielbc@gmail.com>
2025-02-21 00:30:23 -0300

    Alterar Fonte do sistema

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_fest/router.dart';
 import 'package:my_fest/ui/style/pallete.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('/assets/fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(const MyApp());
 }
 
@@ -16,6 +24,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'My Fest',
       theme: ThemeData(
+        fontFamily: GoogleFonts.dosis().fontFamily,
         useMaterial3: true,
         colorScheme:
             ColorScheme.fromSwatch(primarySwatch: Pallete.primaryColor),

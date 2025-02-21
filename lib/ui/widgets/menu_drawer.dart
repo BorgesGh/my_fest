1b32da0f201e6bf6ce7810d3fac89261c63a2f08
BorgesGh <ghabrielbc@gmail.com>
2025-02-21 00:29:55 -0300

    Criar Menu Drawer

---
+++ lib/ui/widgets/menu_drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:my_fest/ui/style/fonts.dart';
+import 'package:my_fest/ui/style/pallete.dart';
+
+class MenuDrawer extends StatelessWidget {
+  const MenuDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      elevation: 10,
+      width: MediaQuery.of(context).size.width * 0.70,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Pallete.primaryColor,
+              image: DecorationImage(
+                  image: AssetImage(
+                'assets/fest.webp',
+              )),
+            ),
+            curve: Curves.linear,
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            title: const Text(
+              'Insumos',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            leading: const ImageIcon(
+              AssetImage('assets/garfo-faca.webp'),
+              color: Pallete.primaryColor,
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

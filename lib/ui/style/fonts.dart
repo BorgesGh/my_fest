79fb81b2d07f01bee8d7434d5083cc2aa57fdcb5
BorgesGh <ghabrielbc@gmail.com>
2025-02-21 00:28:49 -0300

    Criar arquivos de fontes

---
+++ lib/ui/style/fonts.dart
@@ -0,0 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
+
+class Fonts {
+  static final peralta = GoogleFonts.peralta().fontFamily;
+  static final dosis = GoogleFonts.dosis().fontFamily;
+}

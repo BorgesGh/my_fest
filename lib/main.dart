c8aff81fb225b91623053d446dc78381c11ddec9
BorgesGh <ghabrielbc@gmail.com>
2025-03-24 22:08:10 -0300

    feat: add ProductBloc to Provider App

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:my_fest/data/persistence/local/database_helper.dart';
 import 'package:my_fest/router.dart';
+import 'package:my_fest/ui/controller/product_list/product_list_bloc.dart';
+import 'package:my_fest/ui/controller/product_list/product_list_event.dart';
 import 'package:my_fest/ui/style/pallete.dart';
+import 'package:my_fest/utils/assets.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +17,8 @@ void main() {
     final license = await rootBundle.loadString('/assets/fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
+
+  Assets.loadAssets();
 
   DatabaseHelper().database;
 
@@ -24,15 +30,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: goRouter(),
-      debugShowCheckedModeBanner: false,
-      title: 'My Fest',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.dosis().fontFamily,
-        useMaterial3: true,
-        colorScheme:
-            ColorScheme.fromSwatch(primarySwatch: Pallete.primaryColor),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ProductListBloc>(
+          create: (_) => ProductListBloc()..add(const ProductListLoad()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: goRouter(),
+        debugShowCheckedModeBanner: false,
+        title: 'My Fest',
+        theme: ThemeData(
+          fontFamily: GoogleFonts.dosis().fontFamily,
+          useMaterial3: true,
+          colorScheme:
+              ColorScheme.fromSwatch(primarySwatch: Pallete.primaryColor),
+        ),
       ),
     );
   }
